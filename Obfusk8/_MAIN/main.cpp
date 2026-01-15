cad840d9b201d943164e00240113e12bc6161092
x86byte <111459558+x86byte@users.noreply.github.com>
2026-01-15 18:41:41 +0100

    clang-cl warns & clarity and efficiency: fixed

--- Obfusk8/_MAIN/main.cpp
+++ Obfusk8/_MAIN/main.cpp
@@ -9,58 +9,56 @@ using namespace std;
 void
 REG_CALLE(RegistryAPI* _RegistryAPI)
 {
-        HKEY            hKey;
-        DWORD           dwDisposition;
-        char            readBuffer[256];
-        DWORD           bufferSize          = sizeof(readBuffer);
-        DWORD           type = 0;
+    char            readBuffer[256];
+    HKEY            hKey;
+    DWORD           bufferSize = sizeof(readBuffer), dwDisposition, type = 0;
 
-        LSTATUS status = _RegistryAPI->pRegCreateKeyExA(
-            HKEY_CURRENT_USER,
-            "Software\\Obfusk8",
-            0,
-            NULL,
-            REG_OPTION_NON_VOLATILE,
-            KEY_ALL_ACCESS,
-            NULL,
-            &hKey,
-            &dwDisposition
-        );
+    LSTATUS status = _RegistryAPI->pRegCreateKeyExA(
+        HKEY_CURRENT_USER,
+        "Software\\Obfusk8",
+        0,
+        NULL,
+        REG_OPTION_NON_VOLATILE,
+        KEY_ALL_ACCESS,
+        NULL,
+        &hKey,
+        &dwDisposition
+    );
 
-        const char* valueData = "_Obfusk8";
-        status = _RegistryAPI->pRegSetValueExA(
-            hKey,
-            "Obfusk8",
-            0,
-            REG_SZ,
-            reinterpret_cast<const BYTE*>(valueData),
-            strlen(valueData) + 1
-        );
+    const char* valueData = "_Obfusk8";
+    status = _RegistryAPI->pRegSetValueExA(
+        hKey,
+        "Obfusk8",
+        0,
+        REG_SZ,
+        reinterpret_cast<const BYTE*>(valueData),
+        strlen(valueData) + 1
+    );
 
-        status = _RegistryAPI->pRegQueryValueExA(
-            hKey,
-            "Obfusk8",
-            NULL,
-            &type,
-            reinterpret_cast<LPBYTE>(readBuffer),
-            &bufferSize
-        );
+    status = _RegistryAPI->pRegQueryValueExA(
+        hKey,
+        "Obfusk8",
+        NULL,
+        &type,
+        reinterpret_cast<LPBYTE>(readBuffer),
+        &bufferSize
+    );
 
-        _RegistryAPI->pRegCloseKey(hKey);
+    _RegistryAPI->pRegCloseKey(hKey);
 }
 
 
-bool _check = false; 
+bool _check = false;
 
-HANDLE 
-check(NetworkingAPI* _NetworkingAPI){
+HANDLE
+check(NetworkingAPI* _NetworkingAPI) {
     HANDLE _handle = _NetworkingAPI->pInternetOpenA(
-                                    OBFUSCATE_STRING("StealthClient/1.0").c_str(),
-                                    INTERNET_OPEN_TYPE_DIRECT,
-                                    NULL, NULL, 0
-                                    );
+        OBFUSCATE_STRING("StealthClient/1.0").c_str(),
+        INTERNET_OPEN_TYPE_DIRECT,
+        NULL, NULL, 0
+    );
     if (!_handle) {
-         _check = false;
+        _check = false;
     }
 
     _check = true;
@@ -68,96 +66,73 @@ check(NetworkingAPI* _NetworkingAPI){
 }
 
 
-void 
+void
 Net_CALLE(NetworkingAPI* _NetworkingAPI)
 {
-        HANDLE _handle = check(_NetworkingAPI);
-        if((_check) && !(_handle)) {
-            if((_NetworkingAPI->pInternetConnectA(
-                                    _handle,
-                                    OBFUSCATE_STRING("198.28.28.1").c_str(),
-                                    INTERNET_DEFAULT_HTTP_PORT,
-                                    NULL, NULL,
-                                    INTERNET_SERVICE_HTTP,
-                                    0, 0
-                                    ))){
-            }
+    HANDLE _handle = check(_NetworkingAPI);
+    if ((_check) && !(_handle)) {
+        if ((_NetworkingAPI->pInternetConnectA(
+            _handle,
+            OBFUSCATE_STRING("198.28.28.1").c_str(),
+            INTERNET_DEFAULT_HTTP_PORT,
+            NULL, NULL,
+            INTERNET_SERVICE_HTTP,
+            0, 0
+        ))) {
         }
+    }
 
 }
 
-void 
+void
 Cryp_CALLE(CryptographyAPI* _CryptographyAPI)
 {
-        HCRYPTPROV hProv = 0;
-        HCRYPTHASH hHash = 0;
-        HCRYPTKEY hKey = 0;
-
-        if (!_CryptographyAPI->pCryptAcquireContextA(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
-            if (_CryptographyAPI->pGetLastError && _CryptographyAPI->pGetLastError() == NTE_BAD_KEYSET) {
-                 cout << OBFUSCATE_STRING("NTE_BAD_KEYSET, trying to create a new keyset (for testing only).\n").c_str();
-                 if (!_CryptographyAPI->pCryptAcquireContextA(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_NEWKEYSET))
-                 {
-
-                 }
-            } else {
-
+    HCRYPTPROV hProv = 0, hHash = 0, hKey = 0;
+    if (!_CryptographyAPI->pCryptAcquireContextA(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
+        if (_CryptographyAPI->pGetLastError && _CryptographyAPI->pGetLastError() == NTE_BAD_KEYSET) {
+            cout << OBFUSCATE_STRING("NTE_BAD_KEYSET, trying to create a new keyset (for testing only).\n").c_str();
+            if (!_CryptographyAPI->pCryptAcquireContextA(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_NEWKEYSET))
+            {
             }
         }
-
-        if (!_CryptographyAPI->pCryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash))
-        {
-
+        else {
         }
+    }
 
-        const char* password = OBFUSCATE_STRING("Obfusk8!").c_str();
-        if (!_CryptographyAPI->pCryptHashData(hHash, (const BYTE*)password, (DWORD)strlen(password), 0))
-        {
+    if (!_CryptographyAPI->pCryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash)){}
+    if (!_CryptographyAPI->pCryptHashData(hHash, (const BYTE*)OBFUSCATE_STRING("Obfusk8!").c_str(), (DWORD)strlen(OBFUSCATE_STRING("Obfusk8!").c_str()), 0)){}
+    if (!_CryptographyAPI->pCryptDeriveKey(hProv, CALG_AES_128, hHash, 0, &hKey)){}
 
-        }
+    char originalDataStr[] = "Obfusk8 C++17";
+    std::vector<BYTE> dataBuffer(originalDataStr, originalDataStr + strlen(originalDataStr) + 1);
+    DWORD dataLen = (DWORD)dataBuffer.size();
+    DWORD bufferLen = dataLen;
 
-        if (!_CryptographyAPI->pCryptDeriveKey(hProv, CALG_AES_128, hHash, 0, &hKey))
-        {
+    if (!_CryptographyAPI->pCryptEncrypt(hKey, 0, TRUE, 0, NULL, &bufferLen, 0)){}
 
-        }
+    dataBuffer.resize(bufferLen);
 
-        char originalDataStr[] = "Obfusk8 C++17";
-        std::vector<BYTE> dataBuffer(originalDataStr, originalDataStr + strlen(originalDataStr) + 1);
-        DWORD dataLen = (DWORD)dataBuffer.size();
-        DWORD bufferLen = dataLen;
+    std::vector<BYTE> encryptedBuffer = dataBuffer;
+    memcpy(encryptedBuffer.data(), originalDataStr, dataLen);
+    DWORD encryptedDataLen = dataLen;
 
-        if (!_CryptographyAPI->pCryptEncrypt(hKey, 0, TRUE, 0, NULL, &bufferLen, 0)) 
-        {
-        }
+    if (!_CryptographyAPI->pCryptEncrypt(hKey, 0, TRUE, 0, encryptedBuffer.data(), &encryptedDataLen, (DWORD)encryptedBuffer.size())){}
 
-        dataBuffer.resize(bufferLen);
+    std::vector<BYTE> decryptedBuffer = encryptedBuffer;
+    DWORD decryptedDataLen = encryptedDataLen;
 
-        std::vector<BYTE> encryptedBuffer = dataBuffer;
-        memcpy(encryptedBuffer.data(), originalDataStr, dataLen);
-        DWORD encryptedDataLen = dataLen;
+    if (!_CryptographyAPI->pCryptDecrypt(hKey, 0, TRUE, 0, decryptedBuffer.data(), &decryptedDataLen)) {}
 
-        if (!_CryptographyAPI->pCryptEncrypt(hKey, 0, TRUE, 0, encryptedBuffer.data(), &encryptedDataLen, (DWORD)encryptedBuffer.size()))
-        {
-        }
+    //cout << OBFUSCATE_STRING("CryptDecrypt: SUCCESS\n").c_str();
 
-        std::vector<BYTE> decryptedBuffer = encryptedBuffer;
-        DWORD decryptedDataLen = encryptedDataLen;
+    decryptedBuffer.resize(decryptedDataLen);
 
-        if (!_CryptographyAPI->pCryptDecrypt(hKey, 0, TRUE, 0, decryptedBuffer.data(), &decryptedDataLen)) {
-
-        }
-
-        //cout << OBFUSCATE_STRING("CryptDecrypt: SUCCESS\n").c_str();
-
-        decryptedBuffer.resize(decryptedDataLen);
-
-        //cout << OBFUSCATE_STRING("Decrypted String: ").c_str() << (char*)decryptedBuffer.data() << endl;
+    //cout << OBFUSCATE_STRING("Decrypted String: ").c_str() << (char*)decryptedBuffer.data() << endl;
 }
 
 void
 ProcMan_CALLE(ProcessAPI* _ProcessManipulationAPIs)
 {
-
     DWORD pid = _ProcessManipulationAPIs->pGetCurrentProcessId();
     cout << OBFUSCATE_STRING("-> [ProcessManipulationAPIs - INFO] Current Process ID: ").c_str() << pid << endl;
 
@@ -175,15 +150,14 @@ ProcMan_CALLE(ProcessAPI* _ProcessManipulationAPIs)
 
     cout << OBFUSCATE_STRING("-> [ProcessManipulationAPIs - INFO] Memory allocated at address: ").c_str() << remoteMem << std::endl;
 
-    const char* message = OBFUSCATE_STRING("Obfusk8 C++17").c_str();
     SIZE_T bytesWritten = 0;
-    if (!_ProcessManipulationAPIs->pWriteProcessMemory(hProcess, remoteMem, message, strlen(message) + 1, &bytesWritten)) {
+    if (!_ProcessManipulationAPIs->pWriteProcessMemory(hProcess, remoteMem, OBFUSCATE_STRING("Obfusk8 C++17").c_str(), strlen(OBFUSCATE_STRING("Obfusk8 C++17").c_str()) + 1, &bytesWritten)) {
         std::cerr << OBFUSCATE_STRING("-> [ProcessManipulationAPIs - ERROR] WriteProcessMemory failed with error: ").c_str() << GetLastError() << std::endl;
     }
 
     cout << OBFUSCATE_STRING("-> [ProcessManipulationAPIs - INFO] Wrote ").c_str() << bytesWritten << OBFUSCATE_STRING(" bytes to allocated memory.").c_str() << endl;
 
-    char buffer[1024] = {0};
+    char buffer[1024] = { 0 };
     SIZE_T bytesRead = 0;
     if (!_ProcessManipulationAPIs->pReadProcessMemory(hProcess, remoteMem, buffer, sizeof(buffer), &bytesRead)) {
         cerr << OBFUSCATE_STRING("-> [ProcessManipulationAPIs - ERROR] ReadProcessMemory failed with error: ").c_str() << GetLastError() << endl;
@@ -192,57 +166,58 @@ ProcMan_CALLE(ProcessAPI* _ProcessManipulationAPIs)
     cout << OBFUSCATE_STRING("-> [ProcessManipulationAPIs - INFO] Read ").c_str() << bytesRead << OBFUSCATE_STRING(" bytes from allocated memory: ").c_str() << buffer << endl;
 }
 
-void 
+void
 CALLEE()
 {
-        #pragma region USING
-        // --------------------------------------
-
+#pragma region USING
+    // --------------------------------------
             // ProcessManipulationAPIs
             ProcessAPI* _ProcessManipulationAPIs = new ProcessAPI;
-            if(!(_ProcessManipulationAPIs->IsInitialized() == TRUE)){
+            if (!(_ProcessManipulationAPIs->IsInitialized() == TRUE)) {
                 std::cout << OBFUSCATE_STRING("something are wrong in ProcessManipulationAPIs!! isn't Initialized...\n").c_str();
                 delete _ProcessManipulationAPIs;
-            } else {
+            }
+            else {
                 ProcMan_CALLE(_ProcessManipulationAPIs);
                 delete _ProcessManipulationAPIs;
             }
-
+        
             // CryptographyAPIs
-            CryptographyAPI* _CryptographyAPI =  new CryptographyAPI;
-            if(!( _CryptographyAPI->IsInitialized() == TRUE)){
+            CryptographyAPI* _CryptographyAPI = new CryptographyAPI;
+            if (!(_CryptographyAPI->IsInitialized() == TRUE)) {
                 std::cout << OBFUSCATE_STRING("something are wrong in CryptographyAPI!! isn't Initialized...\n").c_str();
                 delete _CryptographyAPI;
-            } else {
+            }
+            else {
                 Cryp_CALLE(_CryptographyAPI);
                 delete _CryptographyAPI;
             }
-
+        
             // NetworkingAPIs
             NetworkingAPI* _NetworkingAPI = new NetworkingAPI;
-            if(!(_NetworkingAPI->IsInitialized() == TRUE)){
+            if (!(_NetworkingAPI->IsInitialized() == TRUE)) {
                 std::cout << OBFUSCATE_STRING("something are wrong in NetworkingAPI!! isn't Initialized...\n").c_str();
                 delete _NetworkingAPI;
-            } else {
+            }
+            else {
                 Net_CALLE(_NetworkingAPI);
                 delete _NetworkingAPI;
             }
-
+        
             // RegistryAPIs
             RegistryAPI* _RegistryAPI = new RegistryAPI;
-            if(!(_RegistryAPI->IsInitialized() == TRUE)){
+            if (!(_RegistryAPI->IsInitialized() == TRUE)) {
                 std::cout << OBFUSCATE_STRING("something are wrong in RegistryAPIs!! isn't Initialized...\n").c_str();
                 delete _RegistryAPI;
-            } else {
+            }
+            else {
                 REG_CALLE(_RegistryAPI);
                 delete _RegistryAPI;
             }
-
-        // --------------------------------------
-        #pragma endregion USING
+    // --------------------------------------
+#pragma endregion USING
 }
 
-#ifdef STEALTH_API_MAIN
 _main({
           using LoadLibraryA_t = HMODULE(WINAPI*)(LPCSTR);
           using MsgBoxA_t = int(WINAPI*)(HWND, LPCSTR, LPCSTR, UINT);
@@ -256,9 +231,8 @@ _main({
                   STEALTH_API_OBFSTR("user32.dll", "MessageBoxA")
           ) : nullptr;
 
-          if (pMsgBoxA) {
+          if (pMsgBoxA)
               pMsgBoxA(nullptr, OBFUSCATE_STRING("Obfusk8 Library Ready to use :3\n").c_str(), OBFUSCATE_STRING("Ready").c_str(), 0);
-          }
 
           CALLEE();
 
@@ -286,27 +260,20 @@ _main({
           );
 
           if (!status) {
-              cout <<  OBFUSCATE_STRING("-> [SYSCALL - INFO] SUCCESS: ZwOpenProcess handle: ").c_str() << hProcess << endl;
+              cout << OBFUSCATE_STRING("-> [SYSCALL - INFO] SUCCESS: ZwOpenProcess handle: ").c_str() << hProcess << endl;
               K8_SYSCALL("ZwClose", hProcess);
-          } else {
-              cout << OBFUSCATE_STRING("-> [SYSCALL - INFO] FAILED: ZwOpenProcess status: 0x").c_str() << hex << status << endl;
           }
+         else
+                cout << OBFUSCATE_STRING("-> [SYSCALL - INFO] FAILED: ZwOpenProcess status: 0x").c_str() << hex << status << endl;
     })
-#endif
+    /*
 
-/*
+    to hide the main use :
+        _main({})
 
-to hide the main use :
-    _main({})
-        
-to Obfuscte ur apis use :
-    STEALTH_API_OBFSTR
+    to Obfuscte ur apis use :
+        STEALTH_API_OBFSTR
 
-to Obfuscate STRINGS use :
-    OBFUSCATE_STRING
-*/
-
-
-
-
-
+    to Obfuscate STRINGS use :
+        OBFUSCATE_STRING
+    */
