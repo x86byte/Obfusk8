95e4cc80d6f048620da67db50f041ea8eb1a1e8c
x86byte <111459558+x86byte@users.noreply.github.com>
2026-02-07 11:39:26 +0100

    optimize the obfuscation && fixing delay of resolving
    to obfuscation the string every time inside the loop takes a lot of time which is bad for performance..., whoever, this codes needs more of opts

--- Obfusk8/Instrumentation/materialization/transform/getpeb8.hpp
+++ Obfusk8/Instrumentation/materialization/transform/getpeb8.hpp
@@ -26,14 +26,24 @@ NOOPT
 
         while (*str) {
             char c = *str++;
-            if (c >= 'a' && c <= 'z') {
+            if (c >= 'a' && c <= 'z')
                 c = (char)_L_SUB((int)c, 0x20);
-            }
             uint8_t low_byte = (uint8_t)_L_XOR((h & 0xFF), (uint32_t)c);
             uint8_t sub = aes_constexpr::sbox[low_byte];
             h = _L_OR((h >> 8), (h << 24));
             h = _L_XOR(h, (uint32_t)sub);
         }
+        return h;
+    }
+
+    K8_FORCEINLINE uint32_t _char_hasher(char c) {
+        uint32_t h = _BSTRAP_IV;
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+           c = (char)_L_SUB((int)c, 0x20);
+        uint8_t low_byte = (uint8_t)_L_XOR((h & 0xFF), (uint32_t)c);
+        uint8_t sub = aes_constexpr::sbox[low_byte];
+        h = _L_OR((h >> 8), (h << 24));
+        h = _L_XOR(h, (uint32_t)sub);
         return h;
     }
 
@@ -112,28 +122,25 @@ NOOPT
             uint16_t* ords = (uint16_t*)(base + exp->AddressOfNameOrdinals);
 
             uintptr_t targetAddr = 0;
+            uint32_t ssn = 0;
 
             for (uint32_t i = 0; i < exp->NumberOfNames; ++i) {
                 char* name = (char*)(base + names[i]);
                 if (_bstrap_hash(name) == t_hash) {
                     targetAddr = (uintptr_t)(base + funcs[ords[i]]);
-                    break;
-                }
-            }
-
-            if (!targetAddr) return -1;
-
-            uint32_t ssn = 0;
-            for (uint32_t i = 0; i < exp->NumberOfNames; ++i) {
-                char* name = (char*)(base + names[i]);
-                if (name[0] == 'Z' && name[1] == 'w') {
-                    uintptr_t addr = (uintptr_t)(base + funcs[ords[i]]);
-                    if (addr < targetAddr) {
-                        ssn++;
+                    if(!targetAddr) return -1;
+                    for (uint32_t i = 0; i < exp->NumberOfNames; ++i) {
+                        char* name = (char*)(base + names[i]);
+                        if (name && _char_hasher((char)name[0]) == _char_hasher((char)(('y' + 1) - 32)) && _char_hasher((char)name[1]) == _char_hasher((char)('a' + 22)) && base && funcs) {
+                            uintptr_t addr = (uintptr_t)(base + funcs[ords[i]]);
+                            if (addr < targetAddr)
+                                ssn++;
+                        }
                     }
+                    return ssn;
                 }
             }
-            return ssn;
+            return 0;
         }
 
         #pragma pack(push, 1)
@@ -192,12 +199,19 @@ NOOPT
         uint16_t* ords = (uint16_t*)(base + exp->AddressOfNameOrdinals);
 
         void* fnAllocAddr = nullptr;
-        for (uint32_t i = 0; i < exp->NumberOfNames; ++i) {
-            if (_bstrap_hash((char*)(base + names[i])) == _bstrap_hash(OBFUSCATE_STRING("NtAllocateVirtualMemory").c_str()))
-            {
-                fnAllocAddr = (void*)(base + funcs[ords[i]]);
-                break;
-            }
+        // const char* lll = OBFUSCATE_STRING("NtAllocateVirtualMemory").c_str();
+	    for (uint32_t i = 0; i < exp->NumberOfNames; ++i)
+        {
+            auto cc = (char*)(base + names[i]);
+            // NtAllocateVirtualMemory
+		    // ill find a better way to obfuscate this chars && optimize the obfuscation
+    	    if(_char_hasher((char)cc[0]) == _char_hasher((char)('A' + ('D' - 'A') + 10)) && _char_hasher((char)cc[2]) == _char_hasher((char)(cc[7] - 32))
+    		    && _char_hasher((char)cc[10]) == _char_hasher((char)('A' + (86 - 65))) && _char_hasher((char)cc[17]) == _char_hasher((char)(((char)cc[19]) - 32)))
+                {
+    		        // cout << "base -> names : " << (char*)(base + names[i]) << endl;
+                    fnAllocAddr = (void*)(base + funcs[ords[i]]);
+                    break;
+                }
         }
         if (!fnAllocAddr) return 0;
 
